e873879baf02e76680c9c699814f38a57ffc9185
AppVeyor <ci@appveyor.com>
2025-02-07 12:51:56 +0300

    add file

---
+++ README.md
@@ -0,0 +1,5 @@
+### Ahj-RxJS
+
+
+
+[Github Pages]()
---
+++ src/js/Message.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+import { formatDate, formatString } from './utils';
+
+export default class Message {
+  constructor(msg) {
+    this.body = msg.body;
+    this.from = msg.from;
+    this.id = msg.id;
+    this.received = msg.received;
+    this.subject = msg.subject;
+    this.bindToDom();
+  }
+
+  bindToDom() {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+      <span class='autor__message'>${this.from}</span>
+      <span class='body__message'>${formatString(this.subject)}</span>
+      <span class='date__message'>${formatDate(this.received)}</span>
+    `;
+    const messages = document.querySelector('.messages');
+    messages.prepend(div);
+  }
+}
